4333294e6ec9b1fd99321fe1107c94ec267f94ba
Matteo92i <matteo.iovane@gmail.com>
2021-05-30 22:57:29 +0200

    minorenne

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ var etàPass = 65
 if(etàPass >= 65){
     document.getElementById('output').innerHTML = numeroKm * 0.126
 }
+var etàPass = 17
+if(etàPass <= 17){
+    document.getElementById('output').innerHTML = numeroKm * 0.168
+}
