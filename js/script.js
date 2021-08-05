372426efc0aa58050e70556b43bc673577d4798e
Matteo92i <matteo.iovane@gmail.com>
2021-08-05 15:09:42 +0200

    arrotondamento

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-var numeroKm = prompt('numero Km')
-var etàPass = prompt('età passeggero')
-var prezzoAlKm = 0.21
+var numeroKm = prompt('numero Km');
+var etàPass = prompt('età passeggero');
+var prezzoAlKm = 0.21;
+var scontoMag = 40;
+var scontoMin = 20;
 
 
-var scontoMinorenne = 20
-var scontoMaggiorenne = 40
+var scontoMinorenne = 100 - scontoMin;
+var scontoMaggiorenne =  100 - scontoMag;
 
-document.getElementById('prezzo').innerHTML = numeroKm * prezzoAlKm
+
 
 
 if(etàPass >= 65){
-    document.getElementById('output').innerHTML = ((numeroKm * prezzoAlKm) * scontoMaggiorenne) /100
+    document.getElementById('output').innerHTML =  Math.round((((numeroKm * prezzoAlKm) * scontoMaggiorenne) /100) *100) /100
 } else if (etàPass <= 17){
-    document.getElementById('output').innerHTML = ((numeroKm * prezzoAlKm) * scontoMinorenne) /100
-    
-}
+    document.getElementById('output').innerHTML = Math.round((((numeroKm * prezzoAlKm) * scontoMinorenne) /100) *100) /100
+} else{
+    document.getElementById('output').innerHTML = numeroKm * prezzoAlKm
 
+}
     
+
+
 
 
 
